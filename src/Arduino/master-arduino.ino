2c8eee8a197fff44c24c4e2064962a46e93c9646
akarshani ramanayake <chethana.20200288@iit.ac.lk>
2023-04-30 19:46:49 +0530

    Modified the master-slave system

--- src/Arduino/master-arduino.ino
+++ src/Arduino/master-arduino.ino
@@ -1,29 +1,18 @@
 // master
 #include <Wire.h>
-
-const byte LED_PIN = 13;
-
 void setup() {
   Serial.begin(9600);
-  pinMode(LED_PIN, OUTPUT);
-  Wire.begin(9); // address of the second Arduino board
+  Wire.begin(9);
   Wire.onReceive(receiveEvent);
 }
 
 void loop() {
-  // do nothing
 }
 
 void receiveEvent(int byteCount) {
   byte value = Wire.read();
-  if (value == 1) {
-    digitalWrite(LED_PIN, HIGH); // turn on the LED bulb
-  } else {
-    digitalWrite(LED_PIN, LOW); // turn off the LED bulb
-  }
-
-  float h = Wire.read(); // receive humidity value
-  float t = Wire.read(); // receive temperature value
+  float h = Wire.read(); 
+  float t = Wire.read(); 
   int p = Wire.read(); 
   Serial.print(F("Humidity: "));
   Serial.print(h);
